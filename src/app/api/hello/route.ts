5abc04f576d57894ce3e5049d8984a57119ff4da
KotaSC <is0302ke@gmail.com>
2023-04-16 18:00:37 +0900

    refactor: Disable some eslint rule to ignore erros

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,5 @@
-export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export async function GET(request: Request): Promise<Response> {
+  return new Response('Hello, Next.js!');
 }
